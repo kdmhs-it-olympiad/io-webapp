d49ec3a0cfefb9efa8b8beae17c69044507c5af7
JuunKi Jin <me@atko.space>
2019-06-14 00:56:23 +0900

    feat: update home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,11 @@ import { Component, Vue } from 'vue-property-decorator';
   components: {
     Navbar: () => import('@/components/Navbar.vue'),
     Jumbo: () => import('@/components/Jumbo.vue'),
+    TileBox: () => import('@/components/TileBox.vue'),
+    NoticeTile: () => import('@/components/NoticeTile.vue'),
+    EventExampleTile: () => import('@/components/EventExampleTile.vue'),
+
+    Countdown: () => import('@/components/Countdown.vue'),
   },
 })
 class Home extends Vue {
@@ -18,6 +23,13 @@ export default Home;
   <div class="Home">
     <navbar></navbar>
     <jumbo></jumbo>
+    
+    <countdown></countdown>
+
+    <tile-box>
+      <event-example-tile></event-example-tile>
+      <notice-tile></notice-tile>
+    </tile-box>
   </div>
 </template>
 
