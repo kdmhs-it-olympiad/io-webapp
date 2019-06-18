4f8de83cfa23f6519bc6e12fdfc7e99feeee7ab6
a1p4ca <dev.alpaca@gmail.com>
2019-06-18 23:02:44 +0900

    feat: take off a lazy loading of Home

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Home from '@/views/Home.vue';
 
 Vue.use(Router);
 
@@ -10,7 +11,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: () => import('@/views/Home.vue'),
+      component: Home,
       meta: {
         title: '홈',
       },
