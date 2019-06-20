29fa95883578c12a6af501e1727a47cc3b665385
a1p4ca <dev.alpaca@gmail.com>
2019-06-20 23:06:01 +0900

    feat: QnA design (WIP)

--- src/views/QnA.vue
+++ src/views/QnA.vue
@@ -1,22 +1,28 @@
 <script>
 import { Component, Vue } from 'vue-property-decorator';
-import FormText from '@/components/FormText.vue';
+import FormTextarea from '@/components/FormTextarea.vue';
+import FormRow from '@/components/FormRow.vue';
 import IoButton from '@/components/IoButton.vue';
-import PageHero from '@/components/PageHero.vue';
 
 @Component({
   components: {
-    FormText,
+    FormTextarea,
+    FormRow,
     IoButton,
-    PageHero,
   },
 })
 class QnA extends Vue {
-  mounted() {
-    this.$store.commit('updateNavbar', { invert: true });
-  }
+  answers = [];
 
-  ready() {
+  formData = {
+    content: '',
+  };
+
+  formDataValidate = {
+    content: false,
+  };
+
+  submit() {
     alert('준비중입니다.');
   }
 }
@@ -26,13 +32,26 @@ export default QnA;
 
 <template>
   <div class="QnA">
-    <page-hero text="질문과 답변"></page-hero>
     <div class="QnA__content">
       <div class="QnA__section">
         <h2 class="QnA__subheader">질문하기</h2>
         <form @submit.prevent>
-
+          <form-row>
+            <form-textarea
+              v-model="formData.content"
+              :validate="formDataValidate.content"
+              label="등록한 질문은 1~2일 안에 답변과 함께 게시됩니다."
+              error="내용은 필수로 입력해야 합니다"
+              placeholder="질문할 내용을 상세히 적어주세요"
+            ></form-textarea>
+          </form-row>
         </form>
+        <io-button
+          @click="submit"
+          style="float: right; margin-top: 24px;" black
+        >
+          질문 등록
+        </io-button>
       </div>
       <div class="QnA__section">
         <h2 class="QnA__subheader">답변</h2>
@@ -56,15 +75,18 @@ export default QnA;
     &__content {
       display: flex;
       padding: 50px 120px;
+      margin: 0 -40px;
     }
 
     &__section {
       width: 50%;
+      padding: 0 40px;
     }
 
     &__subheader {
       font-size: 36px;
       font-weight: lighter;
+      margin-bottom: 24px;
     }
   }
 
